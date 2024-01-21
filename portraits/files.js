a0f3087f7c4bbf49bcce03581fa82ebc3a7d9cf7
Ian Bicking <ianb@colorstudy.com>
2024-01-21 12:30:33 -0600

    More portraits

--- portraits/files.js
+++ portraits/files.js
@@ -1,5 +1,45 @@
 window.files = window.files || {}; window.files["portraits"] = [
   {
+    "filename": "1930s boy.jpg",
+    "bytes": 55446
+  },
+  {
+    "filename": "1970s teenage boy 2.jpg",
+    "bytes": 45850
+  },
+  {
+    "filename": "1970s teenage boy.jpg",
+    "bytes": 41069
+  },
+  {
+    "filename": "1980s teenage boy.jpg",
+    "bytes": 48652
+  },
+  {
+    "filename": "1980s teenage girl.jpg",
+    "bytes": 60890
+  },
+  {
+    "filename": "afghani woman in burka.jpg",
+    "bytes": 54279
+  },
+  {
+    "filename": "aleutian old man.jpg",
+    "bytes": 85207
+  },
+  {
+    "filename": "aleutian young man.jpg",
+    "bytes": 44284
+  },
+  {
+    "filename": "aleutian young woman.jpg",
+    "bytes": 55029
+  },
+  {
+    "filename": "amazonian young woman.jpg",
+    "bytes": 53249
+  },
+  {
     "filename": "ancient egypt woman 1.jpg",
     "bytes": 154555
   },
@@ -12,6 +52,26 @@ window.files = window.files || {}; window.files["portraits"] = [
     "bytes": 73245
   },
   {
+    "filename": "australian aboriginal young man.jpg",
+    "bytes": 64788
+  },
+  {
+    "filename": "aztec farmer woman 2.jpg",
+    "bytes": 73012
+  },
+  {
+    "filename": "aztec farmer woman.jpg",
+    "bytes": 63404
+  },
+  {
+    "filename": "aztec holy woman young.jpg",
+    "bytes": 70478
+  },
+  {
+    "filename": "aztec holy woman.jpg",
+    "bytes": 76210
+  },
+  {
     "filename": "babylonian man 1.jpg",
     "bytes": 124965
   },
@@ -20,12 +80,20 @@ window.files = window.files || {}; window.files["portraits"] = [
     "bytes": 182004
   },
   {
+    "filename": "berber young woman.jpg",
+    "bytes": 69046
+  },
+  {
     "filename": "bushman man 2.jpg",
     "bytes": 199739
   },
   {
     "filename": "bushman man.jpg",
     "bytes": 182170
+  },
+  {
+    "filename": "catalan teenage boy.jpg",
+    "bytes": 48022
   },
   {
     "filename": "cavewoman 1.jpg",
@@ -40,6 +108,18 @@ window.files = window.files || {}; window.files["portraits"] = [
     "bytes": 116692
   },
   {
+    "filename": "chinese woman.jpg",
+    "bytes": 70449
+  },
+  {
+    "filename": "dark skinned caveman.jpg",
+    "bytes": 51446
+  },
+  {
+    "filename": "dutch afriakner old man.jpg",
+    "bytes": 70375
+  },
+  {
     "filename": "dutch medieval woman.jpg",
     "bytes": 134804
   },
@@ -50,6 +130,22 @@ window.files = window.files || {}; window.files["portraits"] = [
   {
     "filename": "english boy.jpg",
     "bytes": 219386
+  },
+  {
+    "filename": "ethiopian young woman.jpg",
+    "bytes": 80163
+  },
+  {
+    "filename": "flapper woman.jpg",
+    "bytes": 35544
+  },
+  {
+    "filename": "germanic young woman 2.jpg",
+    "bytes": 40339
+  },
+  {
+    "filename": "germanic young woman.jpg",
+    "bytes": 52619
   },
   {
     "filename": "greek woman 2.jpg",
@@ -64,12 +160,32 @@ window.files = window.files || {}; window.files["portraits"] = [
     "bytes": 107710
   },
   {
+    "filename": "gypsy girl.jpg",
+    "bytes": 48792
+  },
+  {
+    "filename": "gypsy woman.jpg",
+    "bytes": 65070
+  },
+  {
     "filename": "han chinese man scholar.jpg",
     "bytes": 136490
   },
   {
     "filename": "han chinese man.jpg",
     "bytes": 93238
+  },
+  {
+    "filename": "hittite girl.jpg",
+    "bytes": 66217
+  },
+  {
+    "filename": "icelandic woman 2.jpg",
+    "bytes": 70630
+  },
+  {
+    "filename": "icelandic woman.jpg",
+    "bytes": 64894
   },
   {
     "filename": "igbo woman 2.jpg",
@@ -80,8 +196,32 @@ window.files = window.files || {}; window.files["portraits"] = [
     "bytes": 183648
   },
   {
+    "filename": "incan young man 2.jpg",
+    "bytes": 60402
+  },
+  {
+    "filename": "incan young man 3.jpg",
+    "bytes": 60402
+  },
+  {
+    "filename": "incan young man.jpg",
+    "bytes": 62175
+  },
+  {
     "filename": "iroquoi teen girl.jpg",
     "bytes": 110902
+  },
+  {
+    "filename": "jamaican woman.jpg",
+    "bytes": 51229
+  },
+  {
+    "filename": "japanese woman poor.jpg",
+    "bytes": 65646
+  },
+  {
+    "filename": "maori old man.jpg",
+    "bytes": 81335
   },
   {
     "filename": "mayan priest man.jpg",
@@ -92,12 +232,48 @@ window.files = window.files || {}; window.files["portraits"] = [
     "bytes": 106722
   },
   {
+    "filename": "medieval farmer woman.jpg",
+    "bytes": 60133
+  },
+  {
+    "filename": "medieval hungarian woman.jpg",
+    "bytes": 50567
+  },
+  {
     "filename": "medieval nun 2.jpg",
     "bytes": 70593
   },
   {
     "filename": "medieval nun.jpg",
     "bytes": 75781
+  },
+  {
+    "filename": "medieval walloon woman.jpg",
+    "bytes": 63926
+  },
+  {
+    "filename": "medieval young Irish man.jpg",
+    "bytes": 46373
+  },
+  {
+    "filename": "medieval young man farmer 2.jpg",
+    "bytes": 57098
+  },
+  {
+    "filename": "medieval young man farmer.jpg",
+    "bytes": 50410
+  },
+  {
+    "filename": "medieval young woman farmer.jpg",
+    "bytes": 63919
+  },
+  {
+    "filename": "mongolian man.jpg",
+    "bytes": 77877
+  },
+  {
+    "filename": "mongolian young man 2.jpg",
+    "bytes": 40479
   },
   {
     "filename": "moor young man.jpg",
@@ -124,8 +300,28 @@ window.files = window.files || {}; window.files["portraits"] = [
     "bytes": 114662
   },
   {
+    "filename": "objibwa young woman.jpg",
+    "bytes": 51694
+  },
+  {
+    "filename": "persian young woman.jpg",
+    "bytes": 39354
+  },
+  {
+    "filename": "polynesian boy.jpg",
+    "bytes": 38060
+  },
+  {
+    "filename": "polynesian girl.jpg",
+    "bytes": 45374
+  },
+  {
     "filename": "roman senator man.jpg",
     "bytes": 193470
+  },
+  {
+    "filename": "russian tartar man.jpg",
+    "bytes": 77903
   },
   {
     "filename": "samoan man.jpg",
@@ -138,5 +334,77 @@ window.files = window.files || {}; window.files["portraits"] = [
   {
     "filename": "samoan woman.jpg",
     "bytes": 143770
+  },
+  {
+    "filename": "shang dynasty woman.jpg",
+    "bytes": 55222
+  },
+  {
+    "filename": "sickly czech medieval man.jpg",
+    "bytes": 50631
+  },
+  {
+    "filename": "tibetan young woman.jpg",
+    "bytes": 55210
+  },
+  {
+    "filename": "tupi woman.jpg",
+    "bytes": 37319
+  },
+  {
+    "filename": "turkish young man.jpg",
+    "bytes": 68228
+  },
+  {
+    "filename": "tutsi woman.jpg",
+    "bytes": 73202
+  },
+  {
+    "filename": "vedic indian young man.jpg",
+    "bytes": 62463
+  },
+  {
+    "filename": "vedic indian young woman.jpg",
+    "bytes": 52800
+  },
+  {
+    "filename": "venetian renaissance young man 2.jpg",
+    "bytes": 40911
+  },
+  {
+    "filename": "venetian renaissance young man.jpg",
+    "bytes": 78519
+  },
+  {
+    "filename": "viet young man.jpg",
+    "bytes": 50048
+  },
+  {
+    "filename": "walloon woman medieval.jpg",
+    "bytes": 61023
+  },
+  {
+    "filename": "welsh old man.jpg",
+    "bytes": 85247
+  },
+  {
+    "filename": "welsh old woman.jpg",
+    "bytes": 50760
+  },
+  {
+    "filename": "welsh woman.jpg",
+    "bytes": 73348
+  },
+  {
+    "filename": "xia dynasty young man.jpg",
+    "bytes": 45331
+  },
+  {
+    "filename": "yoruba young man.jpg",
+    "bytes": 50997
+  },
+  {
+    "filename": "zulu woman.jpg",
+    "bytes": 97395
   }
 ];
